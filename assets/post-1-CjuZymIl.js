c51e8afb5ae39fea428d6aace84de3337e356ce8
xtguan <xtguan@users.noreply.github.com>
2026-01-04 13:56:47 +0000

    deploy: 41ab6d2f23a73e6b4b19f71698ca3acb739a2adb

--- assets/post-1-CjuZymIl.js
+++ assets/post-1-CjuZymIl.js
@@ -6,4 +6,4 @@ This is a sample blog post about a coastal trip.
 ![coast](/src/photos/tampa-clearwater-beach-1.jpg)
 
 More content here. Replace with your own writing.
-`,s={title:"My First Blog Post",date:"2026-01-03",thumbnail:t,content:o};export{s as default};
+`,e={title:"My First Blog Post",date:"2026-01-03",category:"travel",thumbnail:t,content:o};export{e as default};
--- assets/post-2-Dhs6zDTp.js
+++ assets/post-2-Dhs6zDTp.js
@@ -6,4 +6,4 @@ Short travel story sample content with a city photo.
 ![city](/src/photos/tampa-ybor-1.jpg)
 
 More details about the trip.
-`,a={title:"A Travel Story",date:"2026-01-02",thumbnail:t,content:o};export{a as default};
+`,a={title:"A Travel Story",date:"2026-01-02",category:"travel",thumbnail:t,content:o};export{a as default};
